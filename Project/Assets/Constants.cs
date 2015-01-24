d3f476aea4c6c683e8036aabe8e444f280b35289
Chris Varnz <chrisvarnz@gmail.com>
2015-01-24 20:22:07 +0000

    Merged existing C# into Unity project

---
+++ Project/Assets/Constants.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Constants {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public const int kMaxOxygen = 20;
+    public const int kHDTargetPower = 30;
+    public const float kHDDamagedExplosionChance = 0.4f;
+    public const int kMaxCrew = 4;
+
+    public enum Status : int
+    {
+        Broken = 0,
+        Damaged = 1,
+        FullyFunctional = 2
+    };
+}
---
+++ Project/Assets/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public WeaponDef wep_saddr;
+    public int wep_power;
+    public Constants.Status wep_status;
+}
