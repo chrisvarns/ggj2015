721c2ed0f6a14b7a6e351579b2a68a954d77d7e2
Luke 'Milkybar' Shires <luke.milkybar.shires@gmail.com>
2015-01-25 11:06:17 +0000

    Added gui layers

---
+++ Project/Assets/GuiManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiManager : MonoBehaviour 
+{
+    public TextMesh[] gui_lines;
+    public Ship active_ship = null;
+
+
+    // we want to have some kind of state.
+
+	void Start() 
+    {
+        Clear();
+	}
+	
+	void Update() 
+    {
+	    // Input.GetKeyDown(Key) is frame specific key press.
+
+        // jump to the correct state.
+        // active line in the state.
+	}
+
+
+    public void Clear()
+    {
+        foreach (TextMesh text in gui_lines)
+        {
+            text.text = "";
+        }
+    }
+}
